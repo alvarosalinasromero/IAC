53b9088533be719008c071830b1c5bcc9287ffac
alvaro salinas <alvarosalinasromeroo@gmail.com>
2024-12-11 16:28:49 +0100

    commit

--- aws/main.tf
+++ aws/main.tf
@@ -5,6 +5,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "server2" {
-  ami           = "ami-05git edb7c94b324f73c"
+  ami           = "ami-05edb7c94b324f73c"
   instance_type = "t3.micro"
+
+  tags = {
+    Name = "server2"
+  }
 }
