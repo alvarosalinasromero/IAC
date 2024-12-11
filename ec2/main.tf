222c03fd7cbd1a2550e718437fd1bcb054553c3b
alvaro salinas <alvarosalinasromeroo@gmail.com>
2024-12-11 16:47:23 +0100

    commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,12 @@
+# provider is plugin to download the resource
 provider "aws" {
   region     = "eu-north-1"
   access_key = "AKIA5FTZAGOHFCJFXK5Q"
   secret_key = "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"
 }
+
+# resource describe one or more insfrastructure objects. Ex: ec2 instance
+# aws_instance # name local aws_alb # name local
 
 resource "aws_instance" "server2" {
   ami           = "ami-05edb7c94b324f73c"
@@ -11,4 +15,5 @@ resource "aws_instance" "server2" {
   tags = {
     Name = "server2"
   }
+
 }
