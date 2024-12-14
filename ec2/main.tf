fee4b94170ec740039b588b629cc3dc3fe5db8ab
alvaro salinas <alvarosalinasromeroo@gmail.com>
2024-12-14 20:44:52 +0100

    commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,13 @@
+# variables
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 # provider is plugin to download the resource
 provider "aws" {
   region     = "eu-north-1"
-  access_key = "AKIA5FTZAGOHFCJFXK5Q"
-  secret_key = "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"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # resource describe one or more insfrastructure objects. Ex: ec2 instance
@@ -13,7 +18,7 @@ resource "aws_instance" "server2" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "server2"
+    Name = "server4"
   }
 
 }
