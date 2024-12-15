dcdbe7d5ed5aa54ff103470eea42ad63823f6522
alvaro salinas <alvarosalinasromeroo@gmail.com>
2024-12-15 13:06:46 +0100

    eip

--- aws/eip/main.tf
+++ aws/eip/main.tf
@@ -1,6 +1,22 @@
 # Elastic IP
 # address is a static, public IPv4 address designed specifically for the dynamic nature of cloud computing. This feature allows you to associate an Elastic IP address with any instance or network interface within any Virtual Private Cloud (VPC) in your AWS account.
 
-resource "aws_eip" "eip" {
-    instance = aws_instance.server2.id
+# Create a new Elastic IP
+resource "aws_eip" "ipstatic" {
+    domain = "vpc"
+}
+
+# Create a security group
+resource "aws_security_group" "allow_tls" {
+  name        = "myfirewall"
+  description = "Firewall for the server"
+}
+
+# inbound rule to allow traffic with elastic ip
+resource "aws_vpc_security_group_ingress_rule" "sgnew" {
+  security_group_id = aws_security_group.sgnew.id
+  cidr_ipv4         = "${aws_eip.ipstatic.public_ip}/32" # aws_eip.ipstatic.public_ip
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
 }
