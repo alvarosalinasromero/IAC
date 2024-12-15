eaa8a16431b222a46c1e2c6b9a0c8e9b5fa29f70
alvaro salinas <alvarosalinasromeroo@gmail.com>
2024-12-15 12:24:06 +0100

    sg

---
+++ aws/securitygroups/main.tf
@@ -0,0 +1,45 @@
+
+# provider is plugin to download the resource
+provider "aws" {
+  region     = "eu-north-1"
+}
+
+# create a security group
+resource "aws_security_group" "allow_tls" {
+  name        = "myfirewall"
+  description = "Firewall for the server"
+}
+
+# ingress rule is an inbound rule
+# aws_vpc.main.cidr_block range IP address
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 42
+# }
+
+# egress rule is an outbound rule
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
