d3aa0e30045384341344dc8b295eb880dd67ae57
alvaro salinas <alvarosalinasromeroo@gmail.com>
2024-12-15 13:17:53 +0100

    output

---
+++ aws/outputs/main.tf
@@ -0,0 +1,15 @@
+# provider is plugin to download the resource
+provider "aws" {
+  region     = "eu-north-1"
+}
+
+# create a security group
+resource "aws_security_group" "newsg" {
+  name        = "myfirewall"
+  description = "Firewall for the server"
+}
+
+# adding output
+output "security_group_id" {
+  value = aws_security_group.newsg.id
+}
