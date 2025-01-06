635738c2e9af4cbdf5292d390f36d6660cdaddab
alvarosalinasromero <alvarosalinasromeroo@gmail.com>
2025-01-06 15:52:20 +0100

    countindex

---
+++ aws/function/main.tf
@@ -0,0 +1,11 @@
+
+resource "aws_iam_user" "this" {
+  name = "demo-user"
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "demo-user-policy"
+  user = aws_iam_user.this.name
+
+  policy = file ("./policy.json")
+}
