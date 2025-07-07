bcb1d464e4aa3cdfa00e22eb64b0ec0dea3a5f06
alvarosalinas <alvarosalinasromeroo@gmail.com>
2025-07-07 22:09:20 +0200

    added count-loop module

--- count-loop module/instance/variables.tf
+++ count-loop module/instance/variables.tf
@@ -5,6 +5,18 @@ variable "ami" {
   default     = "ami-0fab1b527ffa9b942"
 }
 
+variable "instance_name" {
+  description = "Name of the instance"
+  type        = string
+  default     = "my-instance"
+}
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1
+}
+
 variable "instance_type" {
   description = "Instance type"
   type        = string
--- count-loop module/main.tf
+++ count-loop module/main.tf
@@ -3,18 +3,21 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+module "sg" {
+    source = "./SG"
+}
+
 module "instance" {
   source = "./instance"
+
+  instance_count = 3
+  instance_name = "webserver"
   ami = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = module.sg.security_group_ids
-  tags = {
-    Name = "my-instance"
-    Environment = "dev"
-    Project = "IAC"
-  }
-}
 
-module "sg" {
-    source = "./SG"
+  tags = {
+    Environment = "dev"
+    Project = "IAC-webapp"
+  }
 }
