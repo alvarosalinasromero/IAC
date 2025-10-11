3f12e47ee05af20a2cd60486cd1327b2df18d90e
alvarosalinas <alvarosalinasromeroo@gmail.com>
2025-10-11 16:29:56 +0200

    clear resources

--- lab-2/main.tf
+++ lab-2/main.tf
@@ -11,28 +11,48 @@ resource "aws_vpc" "main" {
 }
 
 #Deploy the private subnets
-resource "aws_subnet" "private_subnets" {
-  for_each          = var.private_subnet
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.cidr)
-  availability_zone = each.value.az
-}
+# resource "aws_subnet" "private_subnets" {
+#   for_each          = var.private_subnet
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.cidr)
+#   availability_zone = each.value.az
+# }
 
 #Deploy the public subnets
-resource "aws_subnet" "public_subnets" {
-  for_each                = var.public_subnets
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value.cidr_index + 100)
-  availability_zone       = each.value.az
-  map_public_ip_on_launch = true
+# resource "aws_subnet" "public_subnets" {
+#   for_each                = var.public_subnets
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value.cidr_index + 100)
+#   availability_zone       = each.value.az
+#   map_public_ip_on_launch = true
+# }
+
+# resource "aws_subnet" "list_subnet" {
+#   vpc_id = aws_vpc.main.id
+#   cidr_block = var.ips["pro"]
+#   availability_zone = var.azs[0]
+# }
+
+# resource "aws_subnet" "list_subnet" {
+#   for_each = var.ips
+#   vpc_id = aws_vpc.main.id
+#   cidr_block = each.value
+#   availability_zone = var.azs[0]
+# }
+
+resource "aws_subnet" "list_subnet" {
+  for_each          = var.subnet_config
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value.ip
+  availability_zone = each.value.az
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-    tags = {
-        igw = "terraform-igw"
-    }
+  tags = {
+    igw = "terraform-igw"
+  }
 }
 
 # Public Route Table
@@ -73,6 +93,6 @@ resource "aws_security_group" "allow_ssh" {
 
   tags = {
     Name = "${var.project}-${var.location}-SecurityGroup"
-    env = "sg-${var.env}"
+    env  = "sg-${var.env}"
   }
 }
--- lab-2/variables.tf
+++ lab-2/variables.tf
@@ -4,15 +4,15 @@ variable "vpc_cidr" {
 }
 
 variable "private_subnet" {
-    type = map(object({
-    az = string
+  type = map(object({
+    az   = string
     cidr = number
-}))
-    default = {
-        private_subnet_1 = { az = "eu-west-1a", cidr = 0 }
-        private_subnet_2 = { az = "eu-west-1b", cidr = 1 }
-        private_subnet_3 = { az = "eu-west-1c", cidr = 2 }
-    }
+  }))
+  default = {
+    private_subnet_1 = { az = "eu-west-1a", cidr = 0 }
+    private_subnet_2 = { az = "eu-west-1b", cidr = 1 }
+    private_subnet_3 = { az = "eu-west-1c", cidr = 2 }
+  }
 }
 
 variable "public_subnets" {
@@ -30,18 +30,46 @@ variable "public_subnets" {
 
 variable "env" {
   description = "Environment"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
-variable "project"{
+variable "project" {
   description = "Project Name"
-  type = string
-  default = "Identidad"
+  type        = string
+  default     = "Identidad"
 }
 
-variable "location"{
+variable "location" {
   description = "Location"
-  type = string
-  default = "eu-west-1a"
+  type        = string
+  default     = "eu-west-1a"
+}
+
+variable "azs" {
+  description = "Enable Multi-AZ"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+variable "ips" {
+  type = map(string)
+  default = {
+    dev = "10.20.0.0/24"
+    pro = "10.50.0.0/24"
+  }
+}
+
+variable "subnet_config" {
+  type = map(any)
+  default = {
+    pro = {
+      ip = "10.0.1.0/16"
+      az = "eu-west-1a"
+    }
+    dev = {
+      ip = "10.0.32.0/16"
+      az = "eu-west-1b"
+    }
+  }
 }
