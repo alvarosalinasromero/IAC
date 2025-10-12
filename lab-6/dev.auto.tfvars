100ae485589a7ec1fe045f9edf3852eba4c78124
alvarosalinas <alvarosalinasromeroo@gmail.com>
2025-10-12 12:17:05 +0200

    push

--- lab-6/dev.auto.tfvars
+++
@@ -1,2 +0,0 @@
-environment = "dev"
-bucket_name = "my-unique-lab-bucket-eu-west-2-dev"
--- lab-6/main.tf
+++ lab-6/main.tf
@@ -3,16 +3,26 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 6.0"
     }
   }
 }
 
-resource "aws_s3_bucket" "lab_bucket" {
-  bucket = "my-unique-lab-bucket-123456"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Name        = "Lab Bucket"
-    Environment = "Dev"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
